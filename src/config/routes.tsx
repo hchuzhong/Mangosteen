3a6297aabb8888597b8f008ee1523f80a454086d
hchuzhong <hchuzhong@163.com>
2023-11-28 20:57:46 +0800

    finish welcome page

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,19 +1,7 @@
 import { RouteRecordRaw } from "vue-router";
-import { Bar } from "../components/Bar";
-import { Foo } from "../components/Foo";
-import { First } from "../components/welcome/First";
-import { Second } from "../components/welcome/Second";
-import { Third } from "../components/welcome/Third";
-import { Forth } from "../components/welcome/Forth";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-    { path: '/', component: Foo },
-    { path: '/about', component: Bar },
-    { path: '/welcome', component: Welcome, children: [
-        { path: '1', component: First },
-        { path: '2', component: Second },
-        { path: '3', component: Third },
-        { path: '4', component: Forth },
-    ]}
+    { path: '/', redirect: '/welcome' },
+    { path: '/welcome', component: Welcome },
 ]
