70caa682a607ba98087ac31eaefc971c32939104
hchuzhong <hchuzhong@163.com>
2023-11-30 21:39:00 +0800

    add svgstore plugin

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,5 @@
 import { defineComponent, ref } from 'vue';
 import s from './Welcome.module.scss';
-import logo from '../assets/icons/mangosteen.svg';
-import pig from '../assets/icons/pig.svg';
-import clock from '../assets/icons/clock.svg';
-import statistics from '../assets/icons/statistics.svg';
-import cloud from '../assets/icons/cloud.svg';
 import { useRouter } from 'vue-router';
 
 export const Welcome = defineComponent({
@@ -12,10 +7,10 @@ export const Welcome = defineComponent({
     const router = useRouter()
     const refCurStep = ref(0)
     const stepConfig = [
-      {icon: pig, text1: '会挣钱', text2: '还要会省钱'},
-      {icon: clock, text1: '每日提醒', text2: '不会漏掉每一笔账单'},
-      {icon: statistics, text1: '数据可视化', text2: '收支一目了然'},
-      {icon: cloud, text1: '云备份', text2: '再也不怕数据丢失', lastStep: true},
+      {icon: 'pig', text1: '会挣钱', text2: '还要会省钱'},
+      {icon: 'clock', text1: '每日提醒', text2: '不会漏掉每一笔账单'},
+      {icon: 'statistics', text1: '数据可视化', text2: '收支一目了然'},
+      {icon: 'cloud', text1: '云备份', text2: '再也不怕数据丢失', lastStep: true},
     ]
     let curStepConfig = ref(stepConfig[refCurStep.value])
     const nextAction = () => {
@@ -31,12 +26,12 @@ export const Welcome = defineComponent({
     return () => (
       <div class={s.wrapper}>
         <header>
-            <img src={logo} alt="logo" />
+            <svg><use xlinkHref="#mangosteen"/></svg>
             <h1>山竹记账</h1>
         </header>
         <main>
           <div class={s.card}>
-            <img src={curStepConfig.value.icon} alt="icon" />
+            <svg><use xlinkHref={'#' + curStepConfig.value.icon}/></svg>
             <h2>{curStepConfig.value.text1}<br/>{curStepConfig.value.text2}</h2>
           </div>
         </main>
