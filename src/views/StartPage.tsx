2f207bbe1cb12e81982f24b2ccb92b181ddbb298
hchuzhong <hchuzhong@163.com>
2023-12-10 15:19:02 +0800

    Finish Center component

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -2,12 +2,18 @@ import { defineComponent } from 'vue';
 import { Button } from '../shared/Button';
 import s from './StartPage.module.scss';
 import { FloatButton } from '../shared/FloatButton';
+import { Center } from '../shared/Center';
+import { Icon } from '../shared/Icon';
 
 export const StartPage = defineComponent({
     setup: (props, context) => {
         const onClick = () => {console.log('hi');}
         return () => (
             <div>
+                <nav>menu</nav>
+                <Center class={s.pig_wrapper}>
+                    <Icon name='pig' class={s.pig} />
+                </Center>
                 <div class={s.button_wrapper}>
                     <Button class={s.button} onClick={onClick}>test</Button>
                 </div>
