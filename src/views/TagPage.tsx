ac5f7a5880404f6702520c61075cc837a4890d95
hchuzhong <hchuzhong@163.com>
2023-12-12 15:27:26 +0800

    wip: Tags page, add emojiList

---
+++ src/views/TagPage.tsx
@@ -0,0 +1,8 @@
+import { defineComponent, PropType } from 'vue';
+import { RouterView } from 'vue-router';
+
+export const TagPage = defineComponent({
+    setup: (props, context) => {
+        return () => (<RouterView />)
+    }
+})
