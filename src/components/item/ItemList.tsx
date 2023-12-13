b7d15c3e5e103a48ac61413292c61b8d5fbf803f
hchuzhong <hchuzhong@163.com>
2023-12-13 21:37:54 +0800

    add classPrefix props to Tabs component

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,15 +1,33 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './ItemList.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
 
 export const ItemList = defineComponent({
-    props: {
-        name: {
-            type: String as PropType<string>
-        }
-    },
     setup: (props, context) => {
+        const refSelected = ref('本月')
         return () => (
-            <div class={s.wrapper}>ItemList</div>
+            <MainLayout>{{
+                title: () => '山竹记账',
+                icon: () => <Icon name='menu' />,
+                default: () => (
+                    <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+                        <Tab name='本月'>
+                            list1
+                        </Tab>
+                        <Tab name='上月'>
+                            list2
+                        </Tab>
+                        <Tab name='今年'>
+                            list3
+                        </Tab>
+                        <Tab name='自定义起始时间'>
+                            list4
+                        </Tab>
+                    </Tabs>
+                )
+            }}</MainLayout>
         )
     }
 })
