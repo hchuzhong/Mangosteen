81718df0da4f10f7335a763897b6b199d16a2528
hchuzhong <hchuzhong@163.com>
2023-12-23 12:51:17 +0800

    add @faker-js/faker and Mock for local development

---
+++ src/mock/mock.tsx
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker'
+import { AxiosRequestConfig } from 'axios';
+
+type Mock = (config: AxiosRequestConfig) => [number, any]
+
+export const mockSession: Mock = (config) => {
+    return [200, {
+        jwt: faker.word.adjective(128)
+    }]
+}
+
+export const mockTagIndex: Mock = (config) => {
+    let id = 0
+    const createId = () => {
+        id += 1
+        return id
+    }
+    const createTag = (n = 1, attrs?: any) =>
+        Array.from({ length: n }).map(() => ({
+            id: createId(),
+            name: faker.lorem.word(),
+            sign: faker.internet.emoji(),
+            kind: config.params.kind,
+            ...attrs
+        }))
+
+    if (config.params.kind === 'expenses') {
+        return [200, { resources: createTag(7) }]
+    } else {
+        return [200, { resources: createTag(20) }]
+    }
+
+}
