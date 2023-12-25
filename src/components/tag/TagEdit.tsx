5cdc87edfda05b65b6bf574a7d379dcc6df765c7
hchuzhong <hchuzhong@163.com>
2023-12-25 16:51:00 +0800

    add Delete Tag function

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -4,13 +4,25 @@ import { TagForm } from './TagForm';
 import { Button } from '../../shared/Button';
 import s from './Tag.module.scss';
 import { BackIcon } from '../../shared/BackIcon';
-import { useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
+import { http } from '../../shared/Http';
+import { Dialog } from 'vant';
 
 export const TagEdit = defineComponent({
     setup: (props, context) => {
-        const route = useRoute()
-        const numberId = parseInt(route.params.id!.toString())
+        const router = useRouter()
+        const numberId = parseInt(router.currentRoute.value.params.id!.toString())
         if (Number.isNaN(numberId)) return () => <div>id 不存在</div>
+        const onDelete = async (option?: { withItem?: boolean }) => {
+            console.log(option);
+            await Dialog.confirm({
+                title: '确认',
+                message: option?.withItem ? '删除标签后，该标签下的所有记账也将被删除，是否继续？' : '是否要删除该标签？'
+            })
+            await http.delete(`/api/tags/${numberId}`, { withItem: option?.withItem ? 'true' : 'false' })
+                .catch(() => Dialog.alert({title: '提示', message: '删除失败'}))
+            router.back()
+        }
         return () => (
             <MainLayout>{{
                 title: () => '编辑标签',
@@ -18,8 +30,8 @@ export const TagEdit = defineComponent({
                 default: () => <>
                     <TagForm id={numberId} />
                     <div class={s.actions}>
-                        <Button class={s.removeTags} level='danger' >删除标签</Button>
-                        <Button class={s.removeTagsAndItems} level='danger' >删除标签和记账</Button>
+                        <Button class={s.removeTags} level='danger' onClick={() => onDelete()} >删除标签</Button>
+                        <Button class={s.removeTagsAndItems} level='danger' onClick={() => onDelete({withItem: true})} >删除标签和记账</Button>
                     </div>
                 </>
             }}</MainLayout>
