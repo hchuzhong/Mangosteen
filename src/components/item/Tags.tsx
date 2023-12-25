963e0303c623fcd36d0abf8064bacfa4239cb9b9
hchuzhong <hchuzhong@163.com>
2023-12-25 15:39:11 +0800

    add long press function

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './Tags.module.scss';
 import { http } from '../../shared/Http';
 import { useTags } from '../../shared/useTags';
@@ -19,8 +19,27 @@ export const Tags = defineComponent({
         const {hasMore, tags, fetchTags} = useTags((page) => {
             return http.get<Resources<Tag>>('/tags', { kind: props.kind, page: page + 1, _mock: 'tagIndex' })
         })
+        const timer = ref<number>()
+        const currentTag = ref<HTMLDivElement>()
+        const onLongPress = () => {
+            clearTimeout(timer.value)
+            console.log('had been long press')
+        }
+        const onTouchStart = (e: TouchEvent) => {
+            timer.value = setTimeout(onLongPress, 600)
+            currentTag.value = e.currentTarget as HTMLDivElement
+        }
+        const onTouchEnd = (e: TouchEvent) => {
+            clearTimeout(timer.value)
+        }
+        const onTouchMove = (e: TouchEvent) => {
+            const pointedElement = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)
+            if (!currentTag.value?.contains(pointedElement) && currentTag.value !== pointedElement) {
+                clearTimeout(timer.value)
+            }
+        }
         return () => <>
-            <div class={s.tags_wrapper}>
+            <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
                 <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
                     <div class={s.sign}>
                         <Icon name="add" class={s.createTag} />
@@ -30,7 +49,9 @@ export const Tags = defineComponent({
                     </div>
                 </RouterLink>
                 {tags.value.map(tag =>
-                    <div class={[s.tag, props.selected === tag.id ? s.selected : '']} onClick={() => context.emit('update:selected', tag.id)}>
+                    <div class={[s.tag, props.selected === tag.id ? s.selected : '']} 
+                        onClick={() => context.emit('update:selected', tag.id)}
+                        onTouchstart={onTouchStart} onTouchend={onTouchEnd}>
                         <div class={s.sign}>
                             {tag.sign}
                         </div>
