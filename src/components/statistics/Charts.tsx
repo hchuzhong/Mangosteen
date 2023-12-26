9d0b6d9d3c44ed85168fd8c4e2c133edc7c8c8c6
hchuzhong <hchuzhong@163.com>
2023-12-26 19:54:57 +0800

    update pie chart logic

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -9,6 +9,8 @@ import { Time } from '../../shared/time';
 
 type DataItem = {happened_at: string, amount: number}
 type Data = DataItem[]
+type DataItem2 = {tag_id: string, amount: number, tag: Tag}
+type Data2 = DataItem2[]
 
 const DAY = 24 * 60 * 60 * 1000
 
@@ -37,10 +39,28 @@ export const Charts = defineComponent({
                 happen_after: props.startDate,
                 happen_before: props.endDate,
                 kind: kind.value,
+                group_by: 'happened_at',
                 _mock: 'itemSummary'
             })
             data.value = response.data.groups
         })
+
+        const data2 = ref<Data2>([])
+        const betterData2 = computed<{name: string, value: number}[]>(() => {
+            return data2.value.map(item => ({name: item.tag.name, value: item.amount}))
+        })
+        onMounted(async () => {
+            if (!props.startDate || !props.endDate) return
+            const response = await http.get<{groups: Data2, summary: number}>('/items/summary', {
+                happen_after: props.startDate,
+                happen_before: props.endDate,
+                kind: kind.value,
+                group_by: 'tag_id',
+                _mock: 'itemSummary'
+            })
+            data2.value = response.data.groups
+        })
+
         return () => (
             <div class={s.wrapper}>
                 <FormItem label='类型' type='select' options={[
@@ -48,7 +68,7 @@ export const Charts = defineComponent({
                     { value: 'income', text: '收入' },
                 ]} v-model={kind.value} />
                 <LineChart data={betterData.value} />
-                <PieChart />
+                <PieChart data={betterData2.value} />
                 <Bars />
             </div>
         )
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -4,23 +4,25 @@ import { AxiosRequestConfig } from 'axios';
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
 export const mockItemSummary: Mock = (config) => {
-    return [200, {
-        groups: [
-            {
-                happened_at: "2023-12-10",
-                amount: 700
-            },
-            {
-                happened_at: "2023-12-15",
-                amount: 300
-            },
-            {
-                happened_at: "2023-12-20",
-                amount: 1100
-            }
-        ],
-        total: 2100
-    }]
+    if (config.params.group_by === 'happened_at') {
+        return [200, {
+            groups: [
+                {happened_at: "2023-12-10", amount: 700},
+                {happened_at: "2023-12-15", amount: 300},
+                {happened_at: "2023-12-20", amount: 1100}
+            ],
+            total: 2100
+        }]
+    } else {
+        return [200, {
+            groups: [
+                {tag_id: 324, amount: 500, tag: {id: 324, name: 'test1'}},
+                {tag_id: 322, amount: 400, tag: {id: 322, name: 'test2'}},
+                {tag_id: 323, amount: 300, tag: {id: 323, name: 'test3'}}
+            ],
+            total: 2100
+        }]
+    } 
 }
 
 export const mockItemIndexBalance: Mock = (config) => {
